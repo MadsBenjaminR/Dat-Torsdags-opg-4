49f1dfc1fc152e270894e483f070a3b5094447a6
Mads Benjamin Ribberholt <benjamin.ribberholt@gmail.com>
2023-09-28 12:03:06 +0200

    Task 1
    Task 1

--- Task 1/Main.java
+++ Task 1/Main.java
@@ -1,6 +1,20 @@
 class Main {
-	
+
 public static void main(String [] args){
-	
+
+Team a = new Team("Team: Argon",1);
+Team b = new Team("Team: Blue",2);
+Team c = new Team("Team: Citron",3);
+Team d = new Team("Team: Dowly",4);
+Team e = new Team("Team: Ebony",5);
+
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+
+
 }
 }
--- Task 1/Team.java
+++ Task 1/Team.java
@@ -1,3 +1,24 @@
 class Team {
+
+ private String tname;
+ private String pname;
+ private int trank;
+
+
+ public Team (String tname, int trank){
+
+	this.tname=tname;
+	this.trank=trank;
+  
+
+}
+public void setRank(int teamRank){
+this.trank=teamRank;
+
+}
+public String toString(){
+	String s = "Hold: "+this.tname+" Rang "+this.trank;
 	
+	return s;
+}
 }
